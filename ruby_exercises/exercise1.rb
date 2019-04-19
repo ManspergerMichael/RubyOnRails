2d1295e313b70f90bc7b18f4f42517e2fa3b7a78
Michael Mansperger <ManspergerMichael@gmail.com>
2019-04-18 18:59:17 -0700

    me learn goodly

---
+++ ruby_exercises/exercise1.rb
@@ -0,0 +1,27 @@
+# comments
+
+=begin
+    this
+    is
+    a 
+    longform
+    comment
+=end
+
+#puts includes a newline
+puts "hello"
+puts "jerk"
+puts "bag"
+
+#print dosn't have a newline
+print "eat"
+print "shit"
+puts ""
+
+#methods
+BEGIN {
+    puts "this is in the begin block"
+}
+END {
+    puts "this is in the end block"
+}
