89741a568b5e51f4469e02ac9e983f68f6569ba2
Michael Mansperger <ManspergerMichael@gmail.com>
2019-04-19 10:57:28 -0700

    distracted

--- ruby_exercises/exercise2.rb
+++ ruby_exercises/exercise2.rb
@@ -12,5 +12,14 @@ puts z
 
 first_name = "Michael"
 last_name = "Mansperger"
+
+#puts = writeLine()
+puts "Your name is "
 puts first_name
 puts last_name
+
+#same as above
+puts "Your name is ",first_name,last_name
+
+#concatination
+puts "your name is "+first_name+last_name
