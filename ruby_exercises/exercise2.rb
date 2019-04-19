6d80397df1455c1a9ba105871eeb0f0d8314e354
Michael Mansperger <ManspergerMichael@gmail.com>
2019-04-19 11:00:03 -0700

    strings

--- ruby_exercises/exercise2.rb
+++ ruby_exercises/exercise2.rb
@@ -14,12 +14,16 @@ first_name = "Michael"
 last_name = "Mansperger"
 
 #puts = writeLine()
-puts "Your name is "
-puts first_name
-puts last_name
+#puts "Your name is "
+#puts first_name
+#puts last_name
 
 #same as above
 puts "Your name is ",first_name,last_name
 
 #concatination
-puts "your name is "+first_name+last_name
+puts "your name is "+first_name+" "+last_name
+
+puts "First name is #{first_name} and last name is #{last_name}"
+#or
+puts "First name is %s and last name is %s" % [first_name, last_name]
