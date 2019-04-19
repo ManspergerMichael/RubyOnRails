0a05bd49d3334a98852fa0e71f7a9f970c4f93b2
Michael Mansperger <ManspergerMichael@gmail.com>
2019-04-19 13:00:07 -0700

    finished video

--- ruby_exercises/Strings.rb
+++ ruby_exercises/Strings.rb
@@ -1,5 +1,22 @@
-x = "codingdojo"
+x = "codingDojo"
 puts x .length
 
 puts x.capitalize
 puts x.downcase
+
+puts x[2]
+
+puts x.include?("Dojo")
+
+#prints if the substring is found
+puts "This word has the word 'Dojo'" if x.include?("Dogo") #parenthisies are optional
+
+y = "Andrea, Kadashia, Destiny, Cory, Jess, Kiami"
+#splits string into array
+puts y.split(",")
+#splits string to array and sets to a string
+puts y.split(",").to_s
+
+z = ""
+puts "Z is empty" if z.empty?
+
