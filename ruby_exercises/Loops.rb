3a7432cfbf333acaa7616954f4bb2fed5e27e58c
Michael Mansperger <ManspergerMichael@gmail.com>
2019-04-30 23:04:19 -0700

    Finished loops

--- ruby_exercises/Loops.rb
+++ ruby_exercises/Loops.rb
@@ -12,3 +12,20 @@ while i < num do
     break if i == 3
     i += 1
 end
+
+#for loop
+for i in 0..5
+    puts "value of local variable is #{i}"
+end
+
+#for loop with break
+for i in 0..5
+    puts "The for loop breaks at i = 3"
+    break if i == 3
+end
+
+#To skip an index
+for i in 0..5
+    next if i == 2
+    puts "This loop skips 2 i = #{i}"
+end
