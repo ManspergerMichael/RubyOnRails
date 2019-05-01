41dbf3eb315a0a0c2b409183dfcc6dbf582b3677
Michael Mansperger <ManspergerMichael@gmail.com>
2019-04-30 19:33:04 -0700

    conditionals pt2

--- ruby_exercises/Conditionals.rb
+++ ruby_exercises/Conditionals.rb
@@ -11,4 +11,40 @@ end
 =end
 puts "x is not 2" if x != 2
 puts "x is greater than 2" if x > 2
+#unless is unique to ruby
 puts "x is not 2" unless x == 2
+
+#Ranges
+#when {logic}
+age = 5
+case age
+when 0..2
+    puts "baby"
+when 3..6 
+    puts "child"
+when 7..12
+    puts "adolesent"
+when 13..18
+    puts "teen"
+else
+    puts "adult"
+end
+
+=begin
+in ruby only 2 things evaluate to false nil and false
+an empty string will return true
+=end
+
+if ""
+    puts "I am positive"
+end
+if 0
+    puts "I am positive"
+end
+
+if nil
+    puts "Negitive"
+end
+unless false
+    puts "Negitive"
+end
