716475445f4183c6547dea90bb1d2eadbaca700d
Michael Mansperger <ManspergerMichael@gmail.com>
2019-05-01 13:38:20 -0700

    arrays pt2

---
+++ ruby_exercises/Arrays.rb
@@ -0,0 +1,17 @@
+#arrays
+a = ["Bill","Patrica","Able"]
+b = [5,6,2,3,4]
+
+puts a[0]
+puts a[1]
+
+#merge two arrays and return it
+x = a + b
+#convert array to string
+puts x.to_s
+
+#puts a+b
+
+#print "b's class is " 
+#puts b.class
+#puts b.shuffle.join("-")
