ab48342de0e104ed57e7a978247845aa312be24e
Michael Mansperger <ManspergerMichael@gmail.com>
2019-05-02 12:42:19 -0700

    finished iterateors

--- ruby_exercises/Iterateors.rb
+++ ruby_exercises/Iterateors.rb
@@ -12,10 +12,33 @@ puts arr.any? {|word| word.length >=9 }
 arr.each {|word| print word, "--"}
 #arr.each {|monster| print monster + " is worth 10 exp"}
 
-# .collect puts each element in a new array with results of block
-
+# .collect returns a new array with results of running block for each element in range
+nums = (1..4).collect { |i| i * i}
+puts nums
+nums = (1..4).collect { |i| i + 2 }
+puts nums
 
 # first 5 prime numbers
 #dosent quite work
 #primes = (1..5).collect { |i|  next if (i % 2 != 0); i}
 #puts primes
+
+# .detect/find returns the first element that evaluates to true in block
+print "The result of .find is: "
+puts (1..100).find{ |i| i % 5 == 0 and i % 3 == 0 }
+print "The result of .detect should be the same: "
+puts (1..100).detect{ |i| i % 5 == 0 and i % 3 == 0 }
+
+# .find_all returns an array that eveluate to true in the block
+O_letter_monsters = arr.find_all{|word| word[0] == 'o'}
+puts "These are the mosters that start with 'O': " + O_letter_monsters.to_s
+
+# .reject filters out elements that evaluate to true in block, returns new array
+Not_O_Monsters = arr.reject{|word| word[0] == 'o'}
+puts "These are the mosters that don't with 'O': " + Not_O_Monsters.to_s
+
+# .upto(limit) iterates block up to the limit number
+#Its a for loop!
+0.upto(10) {|i| print i, " "}
+
+0.upto(255){|i| print i if i % 2 == 0}
