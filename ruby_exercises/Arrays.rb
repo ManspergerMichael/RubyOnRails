6c724da3eb3c9d83a2ad09b94b1b0e256afc7f31
Michael Mansperger <ManspergerMichael@gmail.com>
2019-05-01 17:06:45 -0700

    more arrays

--- ruby_exercises/Arrays.rb
+++ ruby_exercises/Arrays.rb
@@ -1,6 +1,7 @@
 #arrays
 a = ["Bill","Patrica","Able"]
 b = [5,6,2,3,4]
+c = ["Dojo", 4,2]
 
 puts a[0]
 puts a[1]
@@ -12,6 +13,29 @@ puts x.to_s
 
 #puts a+b
 
-#print "b's class is " 
-#puts b.class
-#puts b.shuffle.join("-")
+print "b's class is " 
+puts b.class
+#some array functions
+# .shuffle shuffles elements and returns new array .shuffle! reorders elements in place
+# .join('char') joins all values in array with inputed char and returns it as a string
+#puts b.shuffle.join("-") 
+
+#array math
+#Merge arrays a and b and remove any elements that are in c
+y = (a + b) - c
+puts "Array Math Result #{y.to_s}" 
+
+#delete
+a.delete('Able')
+
+#push
+a.push("Cain")
+
+#pop
+
+#.length
+puts a.length
+
+#alternate create array
+d = %w{Rubber Babby Bungi Bumpers}
+puts d
