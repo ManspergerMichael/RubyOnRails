d79f949c6cf04302c3935d40543af20cf040cc13
Michael Mansperger <ManspergerMichael@gmail.com>
2019-05-07 16:57:07 -0700

    video done

--- ruby_exercises/Range.rb
+++ ruby_exercises/Range.rb
@@ -4,9 +4,12 @@ x = (1..5)
 
 puts "Class Name: #{x.class}"
 puts "It dose include 3!" if x.include? 3
+puts "The last number in the range is " x.last.to_s
 puts "The maximum number in the range is " + x.max.to_s
 puts "The minimum number in the range is " + x.min.to_s
 
 #more complicatd example
+#create range of a to z, convert to an array, shuffle it, convert to string
 y = ('a'..'z')
 puts y.to_a.shuffle.to_s
+
