794c56047e74d5c1c23aab7d57e5ceb0109d0fa1
Michael Mansperger <ManspergerMichael@gmail.com>
2019-05-07 20:03:13 -0700

    yield 2

--- ruby_exercises/Yield_and_block.rb
+++ ruby_exercises/Yield_and_block.rb
@@ -7,3 +7,11 @@ def test
 end
 #method_call { block }
 test { puts 'You are in the block' }
+
+#yield(x) will pass a value to the block
+def test_two
+    yield(5)
+    puts "You are in the method test_two"
+    yield(100)
+end
+test_two{|i| puts "You are in block #{i}"}
