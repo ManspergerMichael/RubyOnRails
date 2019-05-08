161a6f222ef03cdd3f41acd1e5f2cb5ea0d29e48
Michael Mansperger <ManspergerMichael@gmail.com>
2019-05-07 20:13:50 -0700

    part 3

--- ruby_exercises/Yield_and_block.rb
+++ ruby_exercises/Yield_and_block.rb
@@ -11,7 +11,16 @@ test { puts 'You are in the block' }
 #yield(x) will pass a value to the block
 def test_two
     yield(5)
-    puts "You are in the method test_two"
+    puts 'You are in the method test_two'
     yield(100)
 end
 test_two{|i| puts "You are in block #{i}"}
+
+#The calculation is done in the block the method prints the results
+#I can have a generic function that I can use over and over
+def square(num)
+    puts "num is #{num}"
+    puts "yeald(num) has a value of #{yield(num)}"
+end
+
+square(5) { |i| i * i }
